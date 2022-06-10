cda9e23c59762f9790cc1c3eb0a121afb8ea66b3
Samuel Chipana <samuelcraft2003@gmail.com>
2022-06-09 21:56:12 -0500

    HelloGitHub

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Otros Cambios</p>
+        <p>Hola GitHub</p>
       </header>
     </div>
   );

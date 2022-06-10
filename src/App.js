3d276ff056304d172d3c016929cf86880e2dbf23
Oscar <oscartaco2003@gmail.com>
2022-06-09 22:06:20 -0500

    social media

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bueno</p>
-      </header>
+      <Footer />
     </div>
   );
 }
-
 
 export default App;
